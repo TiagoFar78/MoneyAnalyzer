e2743531ad80f067a6d485c1893179441f70e830
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2024-01-07 23:57:37 +0000

    Some progress was made...

--- src/main/java/net/tiagofar78/moneyanalyzer/MoneyAnalyzer.java
+++ src/main/java/net/tiagofar78/moneyanalyzer/MoneyAnalyzer.java
@@ -1,11 +1,31 @@
 package net.tiagofar78.moneyanalyzer;
 
+import java.util.Collection;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.ServicesManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.economy.Economy;
 
 public class MoneyAnalyzer extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		ServicesManager sm = getServer().getServicesManager();
+		
+		Collection<RegisteredServiceProvider<Economy>> economies = this.getServer().getServicesManager().getRegistrations(Economy.class);
+		
+		for (RegisteredServiceProvider<Economy> econ : economies) {
+			sm.unregister(econ);
+			
+			Economy changedProvider = econ.getProvider();
+			
+			sm.register(Economy.class, changedProvider, this, econ.getPriority());
+		}
+	}
+	
+	private Economy createMoneyRegisterEconomy(Economy economy) {
 		
 	}
 	
